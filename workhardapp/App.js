c627c83ea67af41393587069a76a337ecfe38de1
Taeyon Kim <taeyon998@gmail.com>
2024-01-28 06:23:18 +0000

    save

--- workhardapp/App.js
+++ workhardapp/App.js
@@ -1,17 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { theme } from '../colors';
+import React, {useState} from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text===""){
+      return;
+    }
+    const newToDos = {...toDos, [Date.now()] : {text, work:working},}
+    setToDos(newToDos);
+    setText(""); // Reset once submitted
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={() => setWorking(true)}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
         </TouchableOpacity>
-        <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={() => setWorking(false)}>
+          <Text style={{...styles.btnText, color: working ? theme.grey : "white"}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+      <View>
+          <TextInput
+            placeholder={working ? "Add a TODO" : 'Where do you want to go?'} 
+            style={styles.input}
+            placeholderTextColor={styles.btnText.grey}
+            onChangeText={onChangeText}
+            onSubmitEditing={addToDo}
+            returnKeyType='done'
+          />
+          <ScrollView>
+            {Object.keys(toDos).map(key => 
+            <View style={styles.toDo}> key={key} 
+              <Text style={styles.toDoText}>{toDos[key.text]}</Text>
+            </View>)
+            }
+          </ScrollView>
+        </View>
     </View>
   );
 }
@@ -30,6 +62,17 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
+    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  },
+  toDoText: {
     color: "white",
   },
 
